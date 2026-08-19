926448240bcfb8a97dabadbed38a755a7d3f0c8f
Brandon-0717 <brandon0717.cr@gmail.com>
2026-08-19 00:28:15 -0600

    act brandon

--- src/Battle.gd
+++ src/Battle.gd
@@ -22,6 +22,8 @@ var current_player_health = 0
 var current_enemy_health = 0
 var is_defending = false
 var is_enemy_defending = false
+var current_player_mp = 0
+var is_reflex_active = false
 
 func _ready():
 	AiMemory.reset()
@@ -29,9 +31,12 @@ func _ready():
 	set_health($EnemyContainer/ProgressBar, enemy.health, enemy.health)
 	set_health($PlayerPanel/PlayerData/ProgressBar, State.current_health, State.max_health)
 	$EnemyContainer/Enemy.texture = enemy.texture
+	
+	set_mp($PlayerPanel/PlayerData/ProgressBar2, State.current_mp, State.max_mp)
 
 	current_player_health = State.current_health
 	current_enemy_health = enemy.health
+	current_player_mp = State.current_mp
 
 	$Textbox.hide()
 	$ActionsPanel.hide()
@@ -47,6 +52,11 @@ func set_health(progress_bar, health, max_health):
 	progress_bar.max_value = max_health
 	progress_bar.get_node("Label").text = "HP: %d/%d" % [health, max_health]
 
+func set_mp(progress_bar, mp, max_mp):
+	# Actualiza la barra visual y el texto de MP.
+	progress_bar.value = mp
+	progress_bar.max_value = max_mp
+	progress_bar.get_node("Label").text = "MP: %d/%d" % [mp, max_mp]
 
 func _input(event):
 	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)) and $Textbox.visible:
@@ -178,17 +188,136 @@ func _enemy_do_attack() -> void:
 		await $AnimationPlayer.animation_finished
 		display_text("You defended successfully!")
 		await self.textbox_closed
-	else:
-		current_player_health = max(0, current_player_health - enemy.damage)
-		set_health($PlayerPanel/PlayerData/ProgressBar, current_player_health, State.max_health)
-		$AnimationPlayer.play("shake")
-		await $AnimationPlayer.animation_finished
-		display_text("%s dealt %d damage!" % [enemy.name, enemy.damage])
-		await self.textbox_closed
+		return
 
+	if is_reflex_active:
+		is_reflex_active = false
+		
+		if randf() < 0.75: # 75% de probabilidad de esquivar
+			$AnimationPlayer.play("mini_shake")
+			await $AnimationPlayer.animation_finished
+			
+			display_text("¡Tu Super Reflejo te permitió esquivar el ataque!")
+			await self.textbox_closed
+			return
+		else:
+			display_text("¡Intentaste esquivar, pero el ataque te alcanzó de todas formas!")
+			await self.textbox_closed
+
+	# Ataque normal / ataque que no pudo esquivar
+	current_player_health = max(0, current_player_health - enemy.damage)
+	set_health(
+		$PlayerPanel/PlayerData/ProgressBar,
+		current_player_health,
+		State.max_health
+	)
+
+	$AnimationPlayer.play("shake")
+	await $AnimationPlayer.animation_finished
+	
+	display_text("%s dealt %d damage!" % [enemy.name, enemy.damage])
+	await self.textbox_closed
 
 func _handle_defeat() -> void:
 	display_text("You were defeated...")
 	await self.textbox_closed
 	await get_tree().create_timer(0.25).timeout
 	get_tree().quit()
+
+#---------SKILLS---------
+func _on_SuperReflejo_pressed():
+	var cost = 20
+	if current_player_mp < cost:
+		close_skills_panel()
+		display_text("¡No tienes suficiente MP!")
+		await self.textbox_closed
+		$ActionsPanel.show()
+		return
+
+	current_player_mp -= cost
+	State.current_mp = current_player_mp
+	set_mp($PlayerPanel/PlayerData/ProgressBar2, current_player_mp, State.max_mp)
+	is_reflex_active = true
+
+	close_skills_panel()
+	display_text("¡Activaste Super Reflejo! Tienes altas probabilidades de esquivar el próximo ataque.")
+	await self.textbox_closed
+
+	enemy_turn()
+
+func _on_GolpeCertero_pressed():
+	var cost = 1
+	if current_player_mp < cost:
+		close_skills_panel()
+		display_text("¡No tienes suficiente MP!")
+		await self.textbox_closed
+		$ActionsPanel.show()
+		return
+
+	current_player_mp -= cost
+	State.current_mp = current_player_mp
+	set_mp($PlayerPanel/PlayerData/ProgressBar2, current_player_mp, State.max_mp)
+
+	close_skills_panel()
+	display_text("¡Preparas un Golpe Certero!")
+	await self.textbox_closed
+
+	if randf() < 0.40:  # 40% de probabilidad de acertar el golpe crítico
+		var crit_damage = int(State.damage * 7) #revisar y considerar el daño base
+		current_enemy_health = max(0, current_enemy_health - crit_damage)
+		set_health($EnemyContainer/ProgressBar, current_enemy_health, enemy.health)
+
+		$AnimationPlayer.play("enemy_damaged")
+		await $AnimationPlayer.animation_finished
+
+		display_text("¡Golpe crítico! Causaste %d de daño!" % crit_damage)
+		await self.textbox_closed
+	else:
+		display_text("¡Tu golpe falló y no logró conectar!")
+		await self.textbox_closed
+
+	if current_enemy_health == 0:
+		display_text("%s was defeated!" % enemy.name)
+		await self.textbox_closed
+
+		$AnimationPlayer.play("enemy_died")
+		await $AnimationPlayer.animation_finished
+
+		await get_tree().create_timer(0.25).timeout
+		get_tree().quit()
+		return
+
+	enemy_turn()
+	
+func _on_Skills_pressed():
+	$ActionsPanel/SkillsPanel.visible = not $ActionsPanel/SkillsPanel.visible
+
+func close_skills_panel():
+	$ActionsPanel/SkillsPanel.hide()
+	
+func _on_CuracionMenor_pressed():
+	var cost = 10
+	if current_player_mp < cost:
+		close_skills_panel()
+		display_text("¡No tienes suficiente MP!")
+		await self.textbox_closed
+		$ActionsPanel.show()
+		return
+
+	current_player_mp -= cost
+	State.current_mp = current_player_mp
+	set_mp($PlayerPanel/PlayerData/ProgressBar2, current_player_mp, State.max_mp)
+
+	var heal_amount = int(State.max_health * 0.25)
+	current_player_health = min(State.max_health, current_player_health + heal_amount)
+	State.current_health = current_player_health
+	set_health($PlayerPanel/PlayerData/ProgressBar, current_player_health, State.max_health)
+
+	close_skills_panel()
+	display_text("¡Te curaste %d puntos de vida!" % heal_amount)
+	await self.textbox_closed
+
+	enemy_turn() #REVISAR QUE PASA DESPUS DE AQUI, YA QUE DESPUES DE CURARNOS EL DRAGON ATACA
+	#Y SI COMENTAMOS ESTO AL FINAL SE QUEDA COMO PEGADO LA PANTALL
+
+#---------FIN SKILLS---------
